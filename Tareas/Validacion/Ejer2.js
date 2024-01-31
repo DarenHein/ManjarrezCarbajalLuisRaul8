979d50741de35e9070cf6e1d7ee5349466046c4c
Daren <darenhein@hotmail.com>
2024-01-31 12:36:29 -0600

    v2

---
+++ Tareas/Validacion/Ejer2.js
@@ -0,0 +1,19 @@
+
+// un sueldo  base = 1000 
+
+// comision es el 10 por por venta 
+// falta ahrehar validaciones de campos 
+function ejer2(){
+
+    let sueldo = "1000" // sueldo sacado de la caja de texto 
+    const porcentaje = 10 
+    let sueldo2 = parseInt(sueldo)
+    let formula = (sueldo2 / porcentaje) * 100
+    let ventas = "4" // tambien pediserla l usuaaio 
+
+
+
+
+
+
+}
