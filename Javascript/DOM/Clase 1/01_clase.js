6860dc4ea9b61d6909da680aba657b5878a0b856
Daren <darenhein@hotmail.com>
2024-02-08 18:52:51 -0600

    agregando documentos doom js

---
+++ Javascript/DOM/Clase 1/01_clase.js
@@ -0,0 +1,15 @@
+
+/**
+ * el doom del js son nodos como un arbol esos nodos pueden tener hijos y esos hijos 
+ * pueden tener mas hijos 
+ * 
+ * el arbol de doom 
+ * trata a document 
+ * la etiqueta html es la etiqueta hija y html tiene dos hijos 
+ * head y body 
+ * head se eneuntran los metadatos 
+ * y en body se encuentran lo normalmente conocemos 
+ */
+
+var obtener = document.querySelector("h1") // obtenermos el contenido de la etiqueta h1 en html 
+obtener.textContent = "hola mundo desde js" establecemos 
