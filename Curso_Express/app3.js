e05ac0f4ebe04012874aca16b625f3f087e35977
Daren_Hein <darenhein@hotmail.com>
2024-05-05 14:56:03 -0600

    creando apartados de carrito y comrpas

---
+++ Curso_Express/app3.js
@@ -0,0 +1,27 @@
+
+/*
+
+	en este apartado ocuapremos el metodo post para leer lo que nuestro 
+	cliente nos pide hay varias formas de leer datos que nos manda el cliente 
+	y tambien el clinet tiene multiples formas de mandar datos 
+*/
+
+const express = require('express')
+const app = express()
+
+// necesitamos los metodos que permitan leer texto json y formularios 
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extend : false}))
+
+// ahroa creamos un metodo post que lee lo que manda el lciente 
+app.post('/cliente' , (req,res) => {
+	console.log(req.body)
+})
+
+
+app.listen(3000 , () => {
+
+	console.log("aplicacion correindo en el puerto 3000" )
+
+})
