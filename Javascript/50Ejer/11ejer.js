80ac3758954f72afeff21fe9a7667673f1b0a822
daren <darenhein@hotmail.com>
2024-07-28 18:45:59 -0600

    actualizando database

--- Javascript/50Ejer/11ejer.js
+++ Javascript/50Ejer/11ejer.js
@@ -1,3 +1,30 @@
 // 11. Escribe una función que verifique si un string es un palíndromo.
 
-// un palindromo es como se escriba una palabra delante por detras se lee de la misma manera 
+// un palindromo es como se escriba una palabra delante por detras se lee de la misma manera
+//
+
+function palindromo(palabra){
+	console.log("palbra original -> " , palabra)
+	// pue es simplre solamente vamos a voltear la cadena original y comparamos 
+	let palabrareversa = []
+	for (let i = palabra.length -1  ; i >= 0 ; i--)[
+		palabrareversa.push(palabra[i])
+	]
+
+	//console.log(palabrareversa)
+	let reversa = ""
+	for (let elementos of palabrareversa){
+		reversa = reversa + elementos
+	}
+
+	if(palabra == reversa){
+		console.log("la palabra es un palindromo")
+	}else{
+		console.log("la palbra no es un palindromo")
+	}
+	console.log("palbra volteada" , reversa) 
+
+    
+}
+
+palindromo("oso")
