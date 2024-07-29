6346feb9a16b75197b77922e631dc3b12d7dc306
daren <darenhein@hotmail.com>
2024-07-28 22:46:42 -0600

    actualizando database

---
+++ Javascript/ejerfunciones/4.js
@@ -0,0 +1,6 @@
+// 5. **Encontrar el número más pequeño**:
+//    - Crea una función flecha que tome un array de números y devuelva el número más pequeño. Asigna esta función a una variable llamada `numeroMasPequeno`.
+
+const mayus = (cadena) => cadena.toUpperCase()
+
+console.log(mayus("hola"))
---
+++ Javascript/ejerfunciones/flecha/5.js
@@ -0,0 +1,9 @@
+// 5. **Encontrar el número más pequeño**:
+//    - Crea una función flecha que tome un array de números y devuelva el número más pequeño. Asigna esta función a una variable llamada `numeroMasPequeno`.
+
+
+const min = (array ) => Math.min(...array)
+
+const array = [1,2,3]
+
+console.log(min(array))
